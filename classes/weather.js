f61271242a25f0b45eb72c99cea6fbfb432fcf09
sofikin <sofia.kalpin@khk.ee>
2021-12-03 11:06:32 +0200

    Display weather data

--- classes/weather.js
+++ classes/weather.js
@@ -4,7 +4,7 @@ class Weather{
         this.key = "77f97b598769bf8861004c74e30ebbba";
     }
     async getWeather(){
-const responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`)
+        const responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`)
         const responceData = await responce.json()
         return responceData
     }
